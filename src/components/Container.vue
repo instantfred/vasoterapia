304e670d0b06e8138f9028e4b802b3ace79013c4
Freddy Ramírez <furede87@gmail.com>
2021-11-11 00:01:39 -0600

    Load questions dynamically from an array

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,26 +1,19 @@
 <template>
   <div class="container">
-    <h1>Vasoterapia</h1>
-    <div class="row">
-      <div class="column" style="background-color:#aaa;">
+    <template v-for="(item, index) in this.questions">
+      <div :key="index" class="column" :style="getColumnColor(index)">
         <FlipCard>
           <template slot="front">
-            <span>Hey loser</span>
+            <span class="card-title">Vasoterapia</span>
+            <h2>#{{index+1}}</h2>
           </template>
           <template slot="back">
-            <span>Thought u could get rid of me loser?</span>
+            <h2>#{{index+1}}</h2>
+            <span class="question">{{item}}</span>
           </template>
         </FlipCard>
       </div>
-      <div class="column" style="background-color:#bbb;">
-        <h2>Column 2</h2>
-        <p>Some text..</p>
-      </div>
-      <div class="column" style="background-color:#ccc;">
-        <h2>Column 3</h2>
-        <p>Some text..</p>
-      </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -31,13 +24,32 @@
     name: 'Container',
     components: {
       FlipCard
+    },
+    data() {
+      return {
+        questions: [
+          "¿Qué buen consejo te ha dado tu mamá?",
+          "¿Cuál es tu mayor temor?",
+          "Cuéntame algo de lo que estés orgullos@",
+          "", "", "", "", "", ""
+        ]
+      }
+    },
+    methods: {
+      getColumnColor(index) {
+        if (index % 2 === 0) {
+          return "background-color:#ccc;";
+        } else {
+          return "background-color:#bbb;";
+        }
+      },
     }
   }
 </script>
 
 <style>
   * {
-  box-sizing: border-box;
+    box-sizing: border-box;
   }
 
   .column {
@@ -51,5 +63,40 @@
     content: "";
     display: table;
     clear: both;
+    margin: 5px;
+  }
+  .card-title {
+    display: inline;
+    position: relative;
+    font: 50px Helvetica, Sans-Serif;
+    letter-spacing: -5px;
+    color: rgba(0,0,255,0.5);
+  }
+
+  .card-title:after {
+    content: "Vasoterapia";
+    position: absolute; left: 3px; top: 1px;
+    color: rgba(255,0,0,0.5);
+  }
+  .center {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+  }
+  @media screen and (max-width: 710px) {
+    .card-title {
+      font: 23px Helvetica, Sans-Serif;
+      letter-spacing: -2px;
+    }
+    .card-title:after {
+      position: absolute; left: 2px; top: 1px;
+      color: rgba(255,0,0,0.5);
+    }
+    .question {
+      font-size: 12px;
+    }
   }
 </style>
